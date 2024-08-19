6b706a0877b23b152f20bbeb338e3c9138371a19
spicysama <a2983352531@outlook.com>
2024-08-19 18:38:38 +0800

    Update info / waveform (#4)
    * Prettier & i18n
    
    * Update info / waveforms
    
    * update ci.yml

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Fish Audio Labeler
+
 [![Build](https://github.com/fishaudio/text-labeler/actions/workflows/ci.yml/badge.svg)](https://github.com/fishaudio/text-labeler/actions/workflows/ci.yml)
 
 [Cloudflare Pages Pages](https://text-labeler.pages.dev/)
 
 ## Run locally / 本地运行
-在 [Actions](https://github.com/fishaudio/text-labeler/actions/workflows/ci.yml) 中可以找到最新的构建产物, 下载并解压后, 直接运行会自动打开浏览器.  
+
+在 [Actions](https://github.com/fishaudio/text-labeler/actions/workflows/ci.yml) 中可以找到最新的构建产物, 下载并解压后, 直接运行会自动打开浏览器.
 
 You can find the latest build artifacts in [Actions](https://github.com/fishaudio/text-labeler/actions/workflows/ci.yml). After downloading and extracting them, running it directly will automatically open the browser.
 
--- src/components/AudioLabeler.tsx
+++ src/components/AudioLabeler.tsx
@@ -5,11 +5,13 @@ import {
   Stack,
   Switch,
   TextField,
+  Snackbar,
 } from "@mui/material";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 import PaperWithPadding from "./PaperWithPadding";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { useWavesurfer } from "@wavesurfer/react";
 
 type AudioLabelerProps = {
   audio: string;
@@ -40,6 +42,9 @@ const AudioLabeler = ({
   const [autoPlay, setAutoPlay] = useState(true);
   const [autoNext, setAutoNext] = useState(true);
   const [tempText, setTempText] = useState(text);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const containerRef = useRef(null);
 
   useEffect(() => {
     setTempText(text);
@@ -78,6 +83,14 @@ const AudioLabeler = ({
     setAutoPlay(getBooleanFromLocalStorage("autoPlay", true));
   }, []);
 
+  const { wavesurfer, isPlaying, currentTime } = useWavesurfer({
+    container: containerRef,
+    height: 100,
+    waveColor: "#0BE3F7",
+    progressColor: "#0497EB",
+    url: audio,
+  });
+
   const onUpdateAutoPlay = (e: any) => {
     if (typeof window !== "undefined") {
       localStorage.setItem("autoPlay", e.target.checked);
@@ -96,16 +109,37 @@ const AudioLabeler = ({
     setTempText(e.target.value);
   };
 
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+
+  const showSnackbar = (message: string) => {
+    setSnackbarMessage(message);
+    setSnackbarOpen(true);
+  };
+
+  const handleTimeUpdate = (e: any) => {
+    wavesurfer?.setTime(e.target.currentTime);
+  };
+
   const disabled = !audio;
 
   return (
     <Stack spacing={2}>
       <PaperWithPadding>
-        <AudioPlayer
-          autoPlay={false}
-          autoPlayAfterSrcChange={autoPlay}
-          src={audio}
-        />
+        <Stack spacing={2}>
+          <AudioPlayer
+            autoPlay={false}
+            autoPlayAfterSrcChange={autoPlay}
+            src={audio}
+            onListen={handleTimeUpdate}
+            onSeeking={handleTimeUpdate}
+            onSeeked={handleTimeUpdate}
+            progressUpdateInterval={100} // ms
+            listenInterval={100} // ms
+          />
+          <div ref={containerRef} style={{ margin: "1.25rem" }} />
+        </Stack>
       </PaperWithPadding>
       <PaperWithPadding>
         <FormGroup row>
@@ -129,7 +163,7 @@ const AudioLabeler = ({
             label="标签文本 / Label Text"
             multiline
             fullWidth
-            rows={2}
+            rows={4}
             onChange={onUpdateText}
             value={tempText}
             disabled={disabled}
@@ -138,20 +172,55 @@ const AudioLabeler = ({
       </PaperWithPadding>
       <PaperWithPadding>
         <Stack spacing={2} direction="row">
-          <Button variant="outlined" fullWidth onClick={() => onPrev(tempText)}>
+          <Button
+            variant="outlined"
+            fullWidth
+            onClick={() => {
+              onPrev(tempText);
+              showSnackbar("已切换到上一个 / Switched to PREV");
+            }}
+          >
             上一个 / PREV
           </Button>
-          <Button variant="outlined" fullWidth onClick={() => onNext(tempText)}>
+          <Button
+            variant="outlined"
+            fullWidth
+            onClick={() => {
+              onNext(tempText);
+              showSnackbar("已切换到下一个 / Switched to NEXT");
+            }}
+          >
             下一个 / NEXT {autoNext ? "(Enter)" : ""}
           </Button>
-          <Button variant="outlined" fullWidth onClick={() => onSave(tempText)}>
+          <Button
+            variant="outlined"
+            fullWidth
+            onClick={() => {
+              onSave(tempText);
+              showSnackbar("已保存 / Saved");
+            }}
+          >
             保存 / SAVE <br /> (Ctrl + S)
           </Button>
-          <Button variant="outlined" fullWidth onClick={onDelete} color="error">
+          <Button
+            variant="outlined"
+            fullWidth
+            onClick={() => {
+              onDelete();
+              showSnackbar("已删除 / Deleted");
+            }}
+            color="error"
+          >
             删除 / DELETE
           </Button>
         </Stack>
       </PaperWithPadding>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleSnackbarClose}
+        message={snackbarMessage}
+      />
     </Stack>
   );
 };
